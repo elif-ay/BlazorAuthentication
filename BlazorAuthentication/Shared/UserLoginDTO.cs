292f4589564a225dde92d89bb9f11d2b44790e67
Elif <Elif@DESKTOP-2JBHN8A>
2022-03-04 14:18:05 +0300

    Proje dosyası ekle.

---
+++ BlazorAuthentication/Shared/UserLoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAuthentication.Shared
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; } = String.Empty;
+
+        public string Password { get; set; } = String.Empty;
+
+    }
+}
